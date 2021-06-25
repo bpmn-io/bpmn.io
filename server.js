b4830cde3b4db6170c4fbb8fa446573b1bd1f067
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2021-06-25 12:42:26 +0200

    feat: serve content with express and st
    This makes it possible to use Heroku as the hosting platform.
    
    Related to https://github.com/bpmn-io/bpmn.io/issues/91

---
+++ server.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const { createServer } = require('./app');
+
+// exit process on SIGINT
+process.on('SIGINT', function() {
+  process.exit();
+});
+
+// start server
+run();
+
+
+// helpers //////////////////////
+
+function now() {
+  return new Date().toString();
+}
+
+function run() {
+
+  const config = getConfig();
+
+  const {
+    port,
+    hostname,
+    env
+  } = config;
+
+  const app = createServer(config);
+
+  app.listen(port, hostname, function() {
+    console.log('[%s] [server] listening at %s:%d (mode: %s)', now(), hostname, port, env);
+
+    if (env === 'development') {
+      const open = require('open');
+
+      open(`http://${hostname}:${port}`);
+    }
+  });
+
+}
+
+function getEnv(name, defaultValue) {
+  return process.env[name] || defaultValue;
+}
+
+function getConfig() {
+
+  const env = getEnv('NODE_ENV', 'production');
+  const port = getEnv('PORT', 8080);
+  const hostname = getEnv('HOST', '0.0.0.0');
+
+  return {
+    port,
+    hostname,
+    env
+  };
+}
